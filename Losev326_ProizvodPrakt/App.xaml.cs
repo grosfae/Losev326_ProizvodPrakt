ac145a4b23bdbbda05c127147e3ae8c3062d804a
Кирилл Лосев <grosfae@outlook.com>
2022-12-13 23:01:09 +0300

    Добавьте файлы проекта.

---
+++ Losev326_ProizvodPrakt/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Losev326_ProizvodPrakt.Components.Model;
+
+namespace Losev326_ProizvodPrakt
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static DbdWikiEntities DB = new DbdWikiEntities();
+        public static User LoggedUser;
+    }
+}   
